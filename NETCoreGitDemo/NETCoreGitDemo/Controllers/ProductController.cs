a9d5eccccccd24ee2675c476cabe8ad9c1a351a8
胡天鸟 <htn@hutianniaodeMacBook.local>
2019-04-02 22:35:24 +0800

    update

---
+++ NETCoreGitDemo/NETCoreGitDemo/Controllers/ProductController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc;
+
+namespace NETCoreGitDemo.Controllers
+{
+    [Route("api/product")]
+    public class ProductController : Controller
+    {
+
+        [HttpGet()]
+        public IActionResult GetProducts()
+        {
+
+            return Content("hello APi !");
+        }
+
+    }
+}
