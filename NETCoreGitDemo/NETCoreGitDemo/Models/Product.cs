ffd97bc24f14491b848b5064230cc1ff34b58b1c
胡天鸟 <htn@hutianniaodeMacBook.local>
2019-04-03 00:29:25 +0800

    add all files

---
+++ NETCoreGitDemo/NETCoreGitDemo/Models/Product.cs
@@ -0,0 +1,10 @@
+﻿using System;
+namespace NETCoreGitDemo.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public float Price { get; set; }
+    }
+}
---
+++ NETCoreGitDemo/NETCoreGitDemo/Services/ProductService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using NETCoreGitDemo.Models;
+
+namespace NETCoreGitDemo.Services
+{
+    public class ProductService
+    {
+        public static ProductService Current { get; } = new ProductService();
+
+        public List<Product> Products { get; }
+
+        public ProductService()
+        {
+            Products = new List<Product>
+            {
+                new Product
+                {
+                Id=1,Name="面包"
+                },
+                new Product
+                {
+                Id=2,Name="牛奶"
+                }
+            };
+        }
+    }
+}
