8d646ab377038f61485775c5fd38409acb468881
胡天鸟 <htn@hutianniaodeMacBook.local>
2019-04-03 13:43:52 +0800

    uodate

--- NETCoreGitDemo/NETCoreGitDemo/Startup.cs
+++ NETCoreGitDemo/NETCoreGitDemo/Startup.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.HttpOverrides;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace NETCoreGitDemo
@@ -27,7 +28,16 @@ namespace NETCoreGitDemo
                 app.UseDeveloperExceptionPage();
             }
 
-            //mvc 和上面addMvc对应
+
+            if(env.IsProduction())
+            {
+                app.UseForwardedHeaders(new ForwardedHeadersOptions
+                {
+                    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+                });
+                
+            }
+
             app.UseMvc();
 
             app.Run(async (context) =>
